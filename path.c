a45302a72f71262cf799e11049f62d6114f74205
Mamsana <alicemamsana@gmail.com>
2022-11-14 10:54:53 -0800

    program that looks for a file in current

---
+++ path.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	int pid = 0;
+
+	for (int i = 0; i < argc; i++)
+	{
+		for (pid = fork() != 0)
+		{
+			printf("Arg%d: %c\n", i, *argv[i]);
+			return (0);
+		}
+	}
+	return (0);
+}
