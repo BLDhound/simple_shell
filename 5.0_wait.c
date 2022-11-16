af53b65576191ac3b3a840d49bd1ba5674bd9fc7
Mamsana <alicemamsana@gmail.com>
2022-11-16 00:01:30 -0800

    updates

--- 5.0_wait.c
+++ 5.0_wait.c
@@ -11,23 +11,23 @@
 int main(void)
 {
 	pid_t child_pid;
-        int status;
+	int status;
 
 	child_pid = fork();
-        if (child_pid == -1)
+	if (child_pid == -1)
 	{
 		perror("Error:");
 		return (1);
-        }
+	}
 	if (child_pid == 0)
 	{
 		printf("Wait for me, wait for me\n");
-	        sleep(3);
+		sleep(3);
 	}
-        else
+	else
 	{
 		wait(&status);
 		printf("Oh, it's all better now\n");
-        }
+	}
 	return (0);
 }
--- 5.1_childpid.c
+++ 5.1_childpid.c
@@ -1,32 +1,53 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void wait()
+/*
+ * main - suspends execution of calling process
+ *
+ * Description: child process has to terminate
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	int i, stat;
 	pid_t pid[5];
 
-	for (i=0; i<5; i++)
-        {
-	        if ((pid[i] = fork()) == 0)
-	        {
+	for (i = 0; i < 5; i++)
+	{
+		((pid[i] = fork()) == 0)
+		{
 			sleep(1);
-	                exit(100 + i);
-	        }
+			exit(100 + i);
+		}
 	}
-	for (i=0; i<5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		pid_t cpid = waitpid(pid[i], &stat, 0);
+
 		if (WIFEXITED(stat))
-	                printf("Child %d terminated with status: %d\n",
-		                   cpid, WEXITSTATUS(stat));
-        }
+		{
+			printf("Child %d terminated with status: %d\n",
+			cpid, WEXITSTATUS(stat));
+		}
+	}
 }
 
-int main()
+#include <stdio.h>
+
+/*
+ * main - checks the system call
+ *
+ * Description: waits for termination
+ *
+ * Returns: Always 0 (success)
+ */
+
+int main(void)
 {
 	wait();
-        return 0;
+	return (0);
 }
