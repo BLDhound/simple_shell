d5d4cb4f92ec0fc3f5c84238114c54dae46d3a5e
Lekarri <lekarrikaila567@gmail.com>
2022-11-17 08:49:39 -0800

    errors fix

--- shell.h
+++ shell.h
@@ -21,7 +21,7 @@ int checker(char **cmd, char *buf);
 void prompt_user(void);
 void handle_signal(int m);
 char **tokenizer(char *line);
-char **test_path(char **path, char *command);
+char *test_path(char **path, char *command);
 char *append_path(char *path, char *command);
 int handle_builtin(char **command, char *line);
 void exit_cmd(char **command, char *line);
