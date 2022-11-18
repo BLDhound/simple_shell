23fd3ab2e135a782515ea012dd4366a0f4069898
Lekarri <lekarrikaila567@gmail.com>
2022-11-18 11:21:04 -0800

    test file fix

--- test_path.c
+++ test_path.c
@@ -1,21 +1,20 @@
 #include "shell.h"
 
 /**
- * test_path - function that checks weather path entered by user is valid
+ * test_path - function that checks if path entered is valid
  * @path: tokenized path
- * @command: user entered
+ * @command: user input
  *
- * Return: path appended with command
- * NULL on failure
+ * Return: path appended, NULL on failure
  */
 char *test_path(char **path, char *command)
 {
 	int i = 0;
-	char *output;
+	char output;
 
 	while (path[i])
 	{
-		output  = append_path(path[i], command);
+		output = append_path(path[i], command);
 		if (access(output, F_OK | X_OK) == 0)
 			return (output);
 		free(output);
